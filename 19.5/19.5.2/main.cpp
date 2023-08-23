50f1daa10f985045c26e72780ef5f253895e8354
Matvey Ushakoff <ushiz.rush@gmail.com>
2023-08-23 07:25:53 +0300

    reworked 19.5.2

--- 19.5/19.5.2/main.cpp
+++ 19.5/19.5.2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 int main(){
 	std::string address;
@@ -13,10 +14,13 @@ int main(){
 	if(!file.is_open()){
 		std::cout << "Wrong address";
 	} else {
-		while(!file.eof()){
-			std::string temp;
-			std::getline(file, temp);
-			std::cout << temp << std::endl;
+		char temp[40];
+		temp[0] = '\0';
+		while (!file.eof()){
+			file.read(temp, 40);
+			for (int i = 0; i < file.gcount(); i++){
+				std::cout << temp[i];
+			}
 		}
 	}
 }
