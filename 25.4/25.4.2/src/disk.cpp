b99cfc0028ca343cdaa95c2268ae3a53ab029eea
Matvey Ushakoff <ushiz.rush@gmail.com>
2023-09-18 20:45:14 +0300

    ended 25.4

--- 25.4/25.4.2/src/disk.cpp
+++ 25.4/25.4.2/src/disk.cpp
@@ -4,21 +4,25 @@
 extern std::vector<int> ram;
 
 void load() {
-  std::ifstream file("files/file.txt");
-  if (!file.eof()) {
+  std::ifstream file("file.txt");
+  if (!file.eof() && file.is_open()) {
     for (int i = 0; i < ram.size(); i++) {
       file >> ram[i];
     }
     std::cout << "Loaded" << std::endl;
-  } else {
+  } else if (file.eof()){
     std::cout << "Disk is empty" << std::endl;
-  }
+  } else {
+		std::cout << "Error with disk" << std::endl;
+	}
+	file.close();
 }
 
 void save() {
-  std::ofstream file("files/file.txt");
+  std::ofstream file("file.txt");
   for (int i = 0; i < ram.size(); i++) {
     file << ram[i];
   }
   std::cout << "Saved" << std::endl;
+	file.close();
 }
--- 25.4/25.4.2/src/main.cpp
+++ 25.4/25.4.2/src/main.cpp
@@ -1,10 +1,40 @@
 #include "cpu.h"
-#include "ram.h"
 #include "disk.h"
 #include "gpu.h"
 #include "kbd.h"
+#include "ram.h"
 
-int main(){
+#include <string>
 
-	return 0;
+void print() {
+  std::cout << "List of commands:" << std::endl;
+  std::cout << "help\nsum\nsave\nload\ninput\ndisplay\nexit\n" << std::endl;
 }
+
+int main() { 
+	extern std::vector<int> ram;
+	print();
+	while(true){
+		std::cout << "Input command: ";
+		std::string command;
+		std::cin >> command;
+
+		if (command == "help"){
+			print();
+		} else if (command == "exit"){
+			break;
+		} else if (command == "sum"){
+			compute();
+		}else if (command == "save"){
+			save();
+		} else if (command == "load"){
+			load();
+		} else if (command == "input"){
+			input();
+		} else if (command == "display"){
+			display();
+		} else {
+			std::cout << "Wrong input" << std::endl;
+		}
+	}
+	return 0; }
